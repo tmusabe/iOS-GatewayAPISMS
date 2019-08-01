b3aa5419b7d7d397b0b33d33aa3d642793ac0910
Taif Al Musabe <taif@nsales.dk>
2019-08-01 14:08:10 +0600

    Initial commit

---
+++ GatewayAPISMS/GatewayAPIWrapper.swift
@@ -0,0 +1,70 @@
+//
+//  GatewayAPIWrapper.swift
+//  nVisionMobile
+//
+//  Created by BS-272 on 29/7/19.
+//  Copyright © 2019 nSales A/S. All rights reserved.
+//
+
+import UIKit
+
+class GatewayAPIWrapper: NSObject {
+    
+    @objc public var sender:String?
+    @objc public var message: String?
+    @objc public var recipients: Array<String>?
+    
+    @objc var delegate: GatewayAPIWrapperDelegate?
+    
+    static private let apiUrl: String = "https://gatewayapi.com/rest/mtsms"
+    
+    private var request: URLRequest!
+    
+    @objc public init(token:String) {
+        super.init()
+        
+        var urlComps = URLComponents(string: GatewayAPIWrapper.apiUrl)!
+        urlComps.queryItems = [URLQueryItem(name: "token", value: token)]
+        
+        self.request = URLRequest(url: urlComps.url!)
+        self.request.httpMethod = "POST"
+        self.request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        self.request.addValue("application/json", forHTTPHeaderField: "Accept")
+    }
+    
+    @objc public func sendSMS() {
+        
+        var recepientsDict : [[String:String]] = []
+        for recepient in recipients! {
+            recepientsDict.append(["msisdn" : recepient])
+        }
+        
+        let json: [String: Any] = ["sender": sender!,
+                                   "message": message!,
+                                   "recipients": recepientsDict
+                                  ]
+        let jsonData = try? JSONSerialization.data(withJSONObject: json)
+        request.httpBody = jsonData
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                self.delegate?.sendSMSError(error: error?.localizedDescription ?? "No Data")
+                print(error?.localizedDescription ?? "No data")
+                return
+            }
+            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
+            if let responseJSON = responseJSON as? [String: Any] {
+                self.delegate?.sendSMSResponse(result: responseJSON)
+                print(responseJSON)
+            }
+        }
+        task.resume()
+        
+    }
+    
+}
+
+@objc protocol GatewayAPIWrapperDelegate {
+    @objc func sendSMSResponse(result: [String: Any])
+    @objc func sendSMSError(error: String)
+}
---
+++ GatewayAPISMS/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  GatewayAPISMS
+//
+//  Created by BS-272 on 1/8/19.
+//  Copyright © 2019 BS-272. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var number: UITextField!
+    @IBOutlet weak var sender: UITextField!
+    @IBOutlet weak var message: UITextView!
+    @IBOutlet weak var video: UIView!
+    
+    
+    func setupAVPlayer() {
+        
+        let videoURL = Bundle.main.url(forResource: "GatewayAPIVideo", withExtension: "mp4") // Get video url
+        let avAssets = AVAsset(url: videoURL!) // Create assets to get duration of video.
+        let avPlayer = AVPlayer(url: videoURL!) // Create avPlayer instance
+        let avPlayerLayer = AVPlayerLayer(player: avPlayer) // Create avPlayerLayer instance
+        avPlayerLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        avPlayerLayer.frame =  video.frame// Set bounds of avPlayerLayer
+        video.layer.addSublayer(avPlayerLayer) // Add avPlayerLayer to view's layer.
+        avPlayer.play() // Play video
+        
+        // Add observer for every second to check video completed or not,
+        // If video play is completed then redirect to desire view controller.
+        avPlayer.addPeriodicTimeObserver(forInterval: CMTime(seconds: 1, preferredTimescale: 1) , queue: .main) { [weak self] time in
+            if time == avAssets.duration {
+                self?.video.isHidden = true
+            }
+        }
+    }
+    
+    //------------------------------------------------------------------------------
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //------------------------------------------------------------------------------
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.setupAVPlayer()  // Call method to setup AVPlayer & AVPlayerLayer to play video
+    }
+
+    @IBAction func sendSMS(_ sender: Any) {
+        let gatewayApi = GatewayAPIWrapper.init(token: "Insert Your Token")
+        gatewayApi.sender = self.sender.text
+        gatewayApi.recipients = [self.number.text] as? Array<String>
+        gatewayApi.message = message.text
+        gatewayApi.sendSMS()
+    }
+
+}
+
+extension ViewController: GatewayAPIWrapperDelegate {
+    func sendSMSResponse(result: [String : Any]) {
+        print(result)
+    }
+    
+    func sendSMSError(error: String) {
+        print(error.description)
+    }
+    
+    
+}
+
